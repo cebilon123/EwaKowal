6cf895fb75d0aa013d1cfafc7759c729b4ce90a9
Michał <michalkowal567@gmail.com>
2020-11-24 13:46:41 +0100

    Changes with padding, added box shadow to elements

--- ewa-kowal/src/components/About.js
+++ ewa-kowal/src/components/About.js
@@ -10,19 +10,13 @@ const About = () => {
                 <p className="Text">
                     Nasz gabinet powstał prawie 20 lat temu i od tamtego czasu 
                     ciągle działamy i pomagamy
-                </p>
-                <PersonInfo>
-                    <PersonInfo.Text>
-                        <div>
-                        Teraz test
-                        </div>
-                    </PersonInfo.Text>
-                    <PersonInfo.Image>
-                        <div>
-                            tutaj niby jakiś image ale na razie test
-                        </div>
-                    </PersonInfo.Image>
-                </PersonInfo>
+                </p>              
+            </section>
+            <section className="PersonInformation">
+                <PersonInfo image="https://picsum.photos/500" text="Jakiś przykładowy tekst który zajemie ileś lini i po polsku żebym ogarnął gdzie się kończą linie i w ogóle. Tutaj Ewa Kowal najlepszy gabinet kosmetyczny ever, wieć zapraszamy, wbijać, mam dwadzieścia lat doświadczenia co znaczy że jestem najlepsza w Rzeszowie. Mam kota, psa, myszy, ryby a nawet kury które są fajne"/>
+                <PersonInfo image="https://picsum.photos/500" text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum"/>
+                <PersonInfo image="https://picsum.photos/500" text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum"/>
+                <div className="Background"/>
             </section>
         </div>
     )
--- ewa-kowal/src/components/reusable/PersonInfo.jsx
+++ ewa-kowal/src/components/reusable/PersonInfo.jsx
@@ -1,26 +1,23 @@
 import React from 'react'
+import '../../styles/PersonInfo.css'
 
 function Text() {
     return null;
 }
 
-function Image() {
-    return null;
-}
 
 class PersonInfo extends React.Component {
     static Text = Text
-    static Image = Image
 
     render() {
-        const { children } = this.props
-        const text = children.find(child => child.type.name === 'Text')
-        const image = children.find(child => child.type.name === 'Image')
-        console.log(text)
         return (
-            <div>
-                PersonInfo reusable class
-                {text ? text.props.children : null}
+            <div className="PersonInfoBox">
+                <div className="TextBox">
+                    <div className="Text">
+                        <div><img className="Image" src={this.props.image} /> </div>
+                        <p>{this.props.text}</p>
+                    </div>
+                </div>
             </div>
         )
     }
