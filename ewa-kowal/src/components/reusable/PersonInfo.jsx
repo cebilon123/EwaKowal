f91ac88f526a866dfc9685ab273ad49033e70b29
Michał <michalkowal567@gmail.com>
2020-11-24 18:11:19 +0100

    Fighting wit styling on various viewports

--- ewa-kowal/src/components/reusable/PersonInfo.jsx
+++ ewa-kowal/src/components/reusable/PersonInfo.jsx
@@ -10,11 +10,13 @@ class PersonInfo extends React.Component {
     static Text = Text
 
     render() {
+        const isLeft = this.props.isLeft ? true : false
+
         return (
             <div className="PersonInfoBox">
                 <div className="TextBox">
                     <div className="Text">
-                        <div><img className="Image" src={this.props.image} /> </div>
+                        <div className={isLeft ? "ImageBox float__left" : "ImageBox float__right"}><img className={isLeft ? "Image__left" : "Image__right"} src={this.props.image} /> </div>
                         <p>{this.props.text}</p>
                     </div>
                 </div>
