d9fc64e222645452cabc3bf143e106ca6ea720b1
Michał <michalkowal567@gmail.com>
2020-11-24 18:24:31 +0100

    Styled about more

--- ewa-kowal/src/components/About.js
+++ ewa-kowal/src/components/About.js
@@ -16,8 +16,8 @@ const About = () => {
                 <PersonInfo image="https://picsum.photos/500" text="Jakiś przykładowy tekst który zajemie ileś lini i po polsku żebym ogarnął gdzie się kończą linie i w ogóle. Tutaj Ewa Kowal najlepszy gabinet kosmetyczny ever, wieć zapraszamy, wbijać, mam dwadzieścia lat doświadczenia co znaczy że jestem najlepsza w Rzeszowie. Mam kota, psa, myszy, ryby a nawet kury które są fajneJakiś przykładowy tekst który zajemie ileś lini i po polsku żebym ogarnął gdzie się kończą linie i w ogóle. Tutaj Ewa Kowal najlepszy gabinet kosmetyczny ever, wieć zapraszamy, wbijać, mam dwadzieścia lat doświadczenia co znaczy że jestem najlepsza w Rzeszowie. Mam kota, psa, myszy, ryby a nawet kury które są fajne"/>
                 <PersonInfo isLeft={true} image="https://picsum.photos/500" text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum"/>
                 <PersonInfo image="https://picsum.photos/500" text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum"/>
-                <div className="Background"/>
             </section>
+                <div className="Background"/>
         </div>
     )
 }
